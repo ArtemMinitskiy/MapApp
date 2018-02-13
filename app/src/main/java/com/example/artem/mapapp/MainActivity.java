8637b7c63fcb018c13a1ebad30b3d2617e72fc63
Minitskiy Artem <rukokuro@gmail.com>
2018-02-13 14:54:06 +0200

    Initial commit

--- app/src/main/java/com/example/artem/mapapp/MainActivity.java
+++ app/src/main/java/com/example/artem/mapapp/MainActivity.java
@@ -1,70 +1,144 @@
 package com.example.artem.mapapp;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
 
-    EditText login, password;
-    DBHelper dbHelper;
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnPoiClickListener {
+
+    private GoogleMap mGoogleMap;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        login = (EditText) findViewById(R.id.login);
-        password = (EditText) findViewById(R.id.password);
-
-        dbHelper = new DBHelper(this);
+        initMap();
 
     }
 
-
-
-    public void addInDB(View view){
-        ContentValues cv = new ContentValues();
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        cv.put("login", login.getText().toString());
-        cv.put("password", password.getText().toString());
-        db.insert("users", null, cv);
-        Log.d("Log", login.getText().toString() + " " + password.getText().toString());
-        dbHelper.close();
-
-        Intent intent = new Intent(MainActivity.this, MapActivity.class);
-        startActivity(intent);
-    }
-
-}
-
-
-
-class DBHelper extends SQLiteOpenHelper {
-
-    public DBHelper(Context context) {
-        super(context, "myDB", null, 1);
+    public void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this); //Устанавливает обратный вызов для фрагмента
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        Log.d("Log", "--- onCreate database ---");
-        db.execSQL("create table users ("
-                + "id integer primary key autoincrement,"
-                + "login text,"
-                + "password text" + ");");
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        try {
+            geoLocate();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        goToLocationZoom();
+        mGoogleMap.setOnPoiClickListener(this);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+
+
+    }
+
+    private void goToLocationZoom() {
+        LatLngBounds ODESSA = new LatLngBounds(new LatLng(46.35, 30.55), new LatLng(46.6, 30.85));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ODESSA.getCenter(), 12));
+        mGoogleMap.setLatLngBoundsForCameraTarget(ODESSA);
+
+    }
+
+    public void geoLocate() throws IOException {
+
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName("London", 1);
+        Address address = list.get(0);
+        String locality = address.getLocality();
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 15);
+        mGoogleMap.moveCamera(update);
+
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void addMarker(View view) {
+        marker = mGoogleMap.addMarker(new MarkerOptions().draggable(true).position(new LatLng(43.5, 30.1)).icon(
+                BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mapTypeNone:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.mapTypeNormal:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mapTypeSatellite:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.mapTypeTerrain:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.mapTypeHybrid:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPoiClick(PointOfInterest poi) {
+        Toast.makeText(getApplicationContext(), poi.name, Toast.LENGTH_LONG).show();
     }
 }
+
